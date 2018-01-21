6013ac748efd8ecac51f50ddbd97a6fc3f659e47
ForestMilles <31958674+ForestMilles@users.noreply.github.com>
2018-01-21 14:24:39 -0500

    changes and additions to project

--- United.py
+++ United.py
@@ -8,9 +8,6 @@ def security():
     else:
         print("Please enter your age")
         
-
-<<<<<<< HEAD
-=======
 def identification():
     choice = input("Please scan your passport by pressing the scan passport button and wait until the blue light turns on. If you want to go back to the info menu please press back.")
     if choice == "scan passport":
@@ -18,7 +15,20 @@ def identification():
     elif choice == "go back":
         info()
 
->>>>>>> 7c99515394acbbe0fddfcc680d475e390e4e788d
+def identification2():
+    choice = input("Please enter you confirmation code on the keypad below or enter you first and last name" )
+    if choice == "keypad":
+        flight()
+    elif choice == "name":
+        name()
+
+def flight():
+    choice = input("you are taking flight 971 to Rekjavik, correct?")
+    if choice == "yes":
+        boardingpass()
+    elif choice == "no":
+        identification2()
+
 def info():
     print("Good morning! Please enter your information or scan your passport. Thank you.")
     choice = input("you can enter your identification through passport or your confirmation code. Please select one option.")
@@ -30,7 +40,6 @@ def info():
         print("you can enter your identification through passport or your confirmation code. Please select one option.")
         info()
             
-<<<<<<< HEAD
 def security():
     age = input("Please enter your age.")
     if age == "<15":
@@ -40,10 +49,5 @@ def security():
         print("Please enter your full name")
     else:
         print("Please enter your age")
-        
-            
-
-        
-=======
+               
 info()
->>>>>>> 7c99515394acbbe0fddfcc680d475e390e4e788d
