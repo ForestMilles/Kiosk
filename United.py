4b28a5c7bb262799f41e5732cbcaa982a6e59b1f
ForestMilles <31958674+ForestMilles@users.noreply.github.com>
2018-03-08 08:45:40 -0500

    updates full of errors!!!

--- United.py
+++ United.py
@@ -1,20 +1,66 @@
 import pygame
 from pygame.locals import *
 
-userface = [960,768]
-def main():
+bright_black = (255,50,0)
+bright_grey = (0,25,0)
+
+display_width = 960
+display_height = 768
+
+Display = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('Checkin Here')
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf',115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        pygame.draw.rect(display, ac, (x, y, w, h))
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
+
+    smallText = pygame.font.Font("freesansbold.ttf", 20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ((x + (w / 2)), (y + (h / 2)))
+    display.blit(textSurf, textRect)
+
+
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def screenintro():
     pygame.init()
-    screen = pygame.display.set_mode(userface)
+    display = pygame.display.set_mode((display_width, display_height))
     while True:
         for e in pygame.event.get():
             if e.type == QUIT:
-                return
+
+                button("Begin Checkin", 150, 450, 100, 50, grey,  bright_grey, button())
+                button("Cancelt", 550, 450, 100, 50, black, bright_black, button())
 
 if __name__ == '__main__':
-    main()
+
+    screenintro()
 
 pygame.quit()
 quit()
+
+
 
 def security():
     age = input("Please enter your age.")
